3edc7d992ee094445b981ba2b7f9a7b119a71074
alex-s4 <al39726867@gmail.com>
2023-06-30 20:47:24 +0800

    Add functions in Service layer

--- src/main/java/com/alexproject/agileninja/services/ProjectService.java
+++ src/main/java/com/alexproject/agileninja/services/ProjectService.java
@@ -1,8 +1,39 @@
 package com.alexproject.agileninja.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.alexproject.agileninja.models.Project;
+import com.alexproject.agileninja.repository.ProjectRepository;
 
 @Service
 public class ProjectService {
 
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	// Create
+	public Project createProject(Project project) {
+		return projectRepository.save(project);
+	}
+	
+	// Read (all and one)
+	public List<Project> findAllProjects(){
+		return projectRepository.findAll();
+	}
+	public Project findProjectById(Long id) {
+		return projectRepository.findById(id).orElse(null);
+	}
+	
+	// Update
+	public Project updateProject(Project project) {
+		return this.projectRepository.save(project);
+	}
+	
+	// Delete
+	public void deleteProject(Long id) {
+		projectRepository.deleteById(id);
+	}
 }
--- src/main/java/com/alexproject/agileninja/services/TicketService.java
+++ src/main/java/com/alexproject/agileninja/services/TicketService.java
@@ -43,5 +43,8 @@ public class TicketService {
 		ticketRepository.deleteById(id);
 	}
 	
+	// Others
+	
+	
 	
 }
